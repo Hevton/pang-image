56d7f672dec7fde6cc3526d4e8960715f8d3c686
hevton <hevton22@gmail.com>
2025-04-12 15:16:42 +0900

    feat: 저장 경로 변경

--- app/src/main/java/io/lib/pang_image/downloader/PangDownloader.kt
+++ app/src/main/java/io/lib/pang_image/downloader/PangDownloader.kt
@@ -52,26 +52,23 @@ object PangDownloader {
 
 
     suspend fun saveImage(
-        context: Context,
         request: PangRequest,
         diskCacheKey: String
-    ): Result<File?> = download(diskCacheKey, request.url, request.cachePath, context)
+    ): Result<File?> = download(diskCacheKey, request.url, request.cachePath)
 
     private suspend fun download(
         cacheKey: String,
         url: String,
         path: String,
-        context: Context,
         downloadDispatcher: CoroutineDispatcher = Dispatchers.IO
     ): Result<File?>  = runCatching {
         val request = Request.Builder().url(url).build()
         withContext(downloadDispatcher) {
             val response = client.newCall(request).execute()
-            val downloadPath = "$path/$cacheKey"
-            val file = File(context.cacheDir, cacheKey)
+            val file = File(path, cacheKey)
 
             response.body()?.byteStream()?.apply {
-                getLock(downloadPath).withLock {
+                getLock(path).withLock {
                     file.outputStream().use { output ->
                         this.copyTo(output)
                     }
